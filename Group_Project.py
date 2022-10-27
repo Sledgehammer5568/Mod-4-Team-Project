42a3ca1e5c88779a847b5a6741eb8fdfa03d289f
Emanuel Ramos <115571450+Sledgehammer5568@users.noreply.github.com>
2022-10-27 14:00:57 -0500

    Outline To Group Work

---
+++ Group_Project.py
@@ -0,0 +1,34 @@
+IN = int(input("""Hello What would you like to do today
+1. option 1
+2. option 2
+3. option 3
+4. option 4
+5. option 5
+6. option 6"""))
+
+if IN == 1:
+    print("SOMETHING 1")
+
+elif IN == 2:
+    print("SOMETHING 2")
+
+elif IN == 3:
+    print("SOMETHING 3")
+
+elif IN == 4:
+    print("SOMETHING 4")
+
+elif IN == 5:
+    print("SOMETHING 5")
+
+elif IN == 6:
+    print("SOMETHING 6")
+
+elif IN == 7:
+    print("SOMETHING 7")
+
+elif IN == 8:
+    print("SOMETHING 8")
+
+else:
+    print("Wrong Input!")
