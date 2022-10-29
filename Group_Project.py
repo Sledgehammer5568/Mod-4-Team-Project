b6d044779a48f3c75c5718f180bc89f6270dd3fd
David Serna <37848056+CelestialGaming@users.noreply.github.com>
2022-10-29 13:39:52 -0500

    Add files via upload
    Changes:
    
    Added two more options, did some organizing with \n

--- Group_Project.py
+++ Group_Project.py
@@ -4,7 +4,9 @@ IN = int(input("""Hello What would you like to do today
 3. option 3
 4. option 4
 5. Play video games 
-6. Eat a potato"""))
+6. Eat a potato
+7. Make a painting
+8. Build a computer"""))
 
 if IN == 1:
     print("SOMETHING 1")
@@ -19,16 +21,16 @@ elif IN == 4:
     print("SOMETHING 4")
 
 elif IN == 5:
-    print("You choose option 5, Play Video games! There are a plethora of different genres of video games. You can play MMOs, RPGs, Rhythm games, Sandbox, Shooters, RTS, and Action-Adventure, and you can also have a combination of all the genres!") #DS's
+    print("You choose option 5, Play Video games! There are a plethora of different genres of video games. \nYou can play MMOs, RPGs, Rhythm games, Sandbox, Shooters, RTS, and Action-Adventure, and you can also have a combination of all the genres!") #We are all gamers - DS
 
 elif IN == 6:
-    print("You choose option 6, Eat a potato! Potatoes are great. You should bake them, chop them up for fries, or make a good meal combo with something else!") #DS's short message
+    print("You choose option 6, Eat a potato! Potatoes are great. You should bake them, chop them up for fries, or make a good meal combo with something else!") #I was hungry when I wrote this for potatoes lol. - Ds
 
 elif IN == 7:
-    print("SOMETHING 7")
+    print("You choose option 7. A good relaxing activity and a chaotic one depending if this is an individual or a group activity. \nThis can be fun if you are challenging yourself to making a self porterate, some inspiration, methods, etc. \nIf it was a group acivity then some chaotic fun like trying something new with others is a good way to discover your outlook with others.") #Thought to add a calming activity lol - DS
 
 elif IN == 8:
-    print("SOMETHING 8")
+    print("You choose option 8. So you want to mentally and physically challenge yourself in the battle of building a personal computer!? Well this option is rightfully descriptive. \nFirst build a list of parts you want to include, YouTube and computer building fourms (like reddit) are your best friend to needing answers to curious questions. \nThen buy the parts which is seemingly the hardest part depending on your financial situation. \nFinally, either get a friend to help you build it or build the PC yourself. \nAgain Youtube is your best friend for any questions and READ the manual for every part if you have too!") #Well, this can be argued that this is a short message. DS
 
 else:
     print("Wrong Input!")
