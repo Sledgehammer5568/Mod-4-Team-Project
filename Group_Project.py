4d2e48664559f61e67594d4d86313db218485219
David Serna <37848056+CelestialGaming@users.noreply.github.com>
2022-10-29 13:12:29 -0500

    DavidSserna-GP
    Edited option 5 and 6 with video game and potato message

--- Group_Project.py
+++ Group_Project.py
@@ -3,8 +3,8 @@ IN = int(input("""Hello What would you like to do today
 2. option 2
 3. option 3
 4. option 4
-5. option 5
-6. option 6"""))
+5. Play video games 
+6. Eat a potato"""))
 
 if IN == 1:
     print("SOMETHING 1")
@@ -19,10 +19,10 @@ elif IN == 4:
     print("SOMETHING 4")
 
 elif IN == 5:
-    print("SOMETHING 5")
+    print("You choose option 5, Play Video games! There are a plethora of different genres of video games. You can play MMOs, RPGs, Rhythm games, Sandbox, Shooters, RTS, and Action-Adventure, and you can also have a combination of all the genres!") #DS's
 
 elif IN == 6:
-    print("SOMETHING 6")
+    print("You choose option 6, Eat a potato! Potatoes are great. You should bake them, chop them up for fries, or make a good meal combo with something else!") #DS's short message
 
 elif IN == 7:
     print("SOMETHING 7")
